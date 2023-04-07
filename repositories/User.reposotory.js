a0f7c4738cb2c68eb817248d4524e61b443a7ed9
shahriarKabir44 <shahriar1904@cseku.ac.bd>
2023-04-08 00:54:59 +0600

    created basic setups

---
+++ repositories/User.reposotory.js
@@ -0,0 +1,40 @@
+const promisify = require('../utils/promisify')
+const queryBuilder = require('../utils/queryBuilder')
+const jwt = require('jsonwebtoken')
+module.exports = class UserRepository {
+    static async findUser({ id }) {
+        let [user] = await promisify({
+            sql: `select name,email, phone, nationality,gender, occupation, profileImageURL
+                from user where id=?;`,
+            values: [id]
+        })
+        return user
+    }
+    static async register({ name, email, phone, nationality, gender, occupation, password }) {
+        await promisify({
+            sql: queryBuilder('user', ['name', 'email', 'phone', 'nationality', 'gender', 'occupation', 'password']),
+            values: [name, email, phone, nationality, gender, occupation, password]
+        })
+        let [user] = await promisify({
+            sql: `select id
+                from user where email=?;`,
+            values: [email]
+        })
+        return user
+    }
+    static async setProfileImage({ id, profileImageURL }) {
+        promisify({
+            sql: `update user set profileImageURL=? where id=?;`,
+            values: [profileImageURL, id]
+        })
+    }
+    static async authenticateUser({ email, password }) {
+        let [user] = await promisify({
+            sql: `select id
+                from user where email=?,password=?;`,
+            values: [email, password]
+        })
+        let token = jwt.sign(user, process.env.jwtSecret)
+        return { user, token }
+    }
+}
--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: process.env.DBPASSWORD,
     database: 'findhome',
     port: 3306
 })
