d67b1a1befd8f5f2f3a458a4786143e66c354f7f
shahriarKabir44 <shahriar1904@cseku.ac.bd>
2023-04-12 12:13:08 +0600

    added more data binding

--- public/js/components/propertyDetails/PropertyDetailsRoot.js
+++ public/js/components/propertyDetails/PropertyDetailsRoot.js
@@ -2,12 +2,18 @@
 async function render() {
     let params = new URLSearchParams(location.search)
     let id = (params.get('id'))
-    let { post } = await fetch('http://localhost:4000/a/' + id)
+    if (id == null) {
+        location.href = 'http://localhost:4000/index.html'
+    }
+    let { property } = await fetch('http://localhost:4000/property/searchPropertybyId/' + id)
         .then(res => res.json())
+    console.log(property)
     return `
 
    
     ${await Navbar()}
+        <br><br><br><br><br>
+
     ${Header()}
     <br><br><br><br><br><br>
     <div class="container-fluid header bg-white p-0">
@@ -15,9 +21,8 @@ async function render() {
 
             <div class="topnav">
                 <a href="index.html">
-                    <h4 class="m-0 text-white">Sky View</h4>
+                    <h4 class="m-0 text-white">Property Details</h4>
                 </a>
-                <a href="propertydetails-sv.html">${post}</a>
                 <a href="#">Floor Plan</a>
                 <a href="#">Location Map</a>
                 <a href="contact.html">Contact</a>
@@ -25,7 +30,6 @@ async function render() {
             </div>
         </div>
     </div>
-    <br><br><br><br><br>
 
 
 
@@ -33,11 +37,7 @@ async function render() {
      <div class="container-fluid header bg-white p-0">
             <div class="row g-0 align-items-center flex-column-reverse flex-md-row">
                 <div class="col-md-6 p-5 mt-lg-5">
-                    <h3 class="display-5 animated fadeIn mb-4">Overview <span class="text-primary">of Sky View</span>
-                    </h3>
-                    <p class="animated fadeIn mb-4 pb-2">Sky View offers Assure Fair View, an attractive residential
-                        property for sale in Mirpur, Dhaka. This property can be a very suitable home address for you.
-                    </p>
+                     
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <h4 class="panel-title mt-4">General Information</h4>
@@ -47,19 +47,19 @@ async function render() {
                                 <tbody>
                                     <tr>
                                         <th><strong>Type</strong></th>
-                                        <td>Commercial</td>
+                                        <td>${property.type}</td>
                                     </tr>
                                     <tr>
                                         <th><strong>Location</strong></th>
-                                        <td>Mirpur, Dhaka, Bangladesh</td>
+                                        <td>${property.location}</td>
                                     </tr>
                                     <tr>
                                         <th><strong>Price</strong></th>
-                                        <td>90,00,000</td>
+                                        <td>BDT ${property.price}</td>
                                     </tr>
                                     <tr>
                                         <th><strong>Unit Size</strong></th>
-                                        <td>1200 Sqft</td>
+                                        <td>${property.area} Sqft</td>
                                     </tr>
 
                                 </tbody>
