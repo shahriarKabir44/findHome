71f119d22fdf694950e5cf5ff8963587855e2edc
shahriarKabir44 <shahriar1904@cseku.ac.bd>
2023-04-14 00:22:32 +0600

    added image editing

--- public/company/propertyDetails/propertyDetails.js
+++ public/company/propertyDetails/propertyDetails.js
@@ -1,8 +1,12 @@
 angular.module('property_details', [])
     .controller('property_details', ($scope) => {
         $scope.images = []
+        $scope.prevewImages = []
         $scope.imageCover = []
-        $scope.availableImagesCount = 0
+        $scope.activeMainImages = 0
+
+
+        $scope.availableImagesCount = []
         $scope.onInit = async () => {
             await $scope.checkLoggedIn()
             let params = new URLSearchParams(location.search)
@@ -13,18 +17,30 @@ angular.module('property_details', [])
             let { property } = await __fetch('property/searchPropertybyId/' + id)
             $scope.images = JSON.parse(property.images)
             $scope.images = $scope.images.map((image, index) => {
-                $scope.availableImagesCount++
-                return { image, cover: 0 }
+                return { image, cover: 0, index }
             })
             if (property == null) {
                 location.href = 'http://localhost:4000/company/dashboard'
             }
+
+            $scope.activeMainImages = $scope.images.length
+
+            console.log($scope.availableImagesCount)
             $scope.property = property
             $scope.$apply()
         }
 
         $scope.toggleCover = image => {
             image.cover ^= 1
+            if (image.cover) {
+                $scope.activeMainImages--
+            }
+            else {
+                $scope.activeMainImages++
+                console.log($scope.activeMainImages + $scope.prevewImages.length)
+                while ($scope.activeMainImages + $scope.prevewImages.length > 3)
+                    $scope.prevewImages.pop()
+            }
         }
 
         $scope.checkLoggedIn = async function () {
@@ -40,5 +56,22 @@ angular.module('property_details', [])
         $scope.confirmUpdate = async () => {
             await __fetch('property/update', $scope.property)
             alert('Updated property')
+        }
+
+        $scope.uploadImage = function (event) {
+            var files = event.target.files;
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $scope.prevewImages.push([e.target.result, $scope.prevewImages.length]);
+
+
+                $scope.$apply();
+            };
+            reader.readAsDataURL(files[0]);
+        };
+
+        $scope.deleteTempImage = index => {
+            console.log(index)
+            $scope.prevewImages = $scope.prevewImages.filter((img, ind) => img[1] != index)
         }
     })
